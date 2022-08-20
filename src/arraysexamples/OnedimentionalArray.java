072176085e9b819914be3e5785ad225126aad274
Prasannaathinarayanan <prasanaamail@gmail.com>
2022-08-20 12:36:59 +0530

    second commit

---
+++ src/arraysexamples/OnedimentionalArray.java
@@ -0,0 +1,19 @@
+package arraysexamples;
+
+public class OnedimentionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int []a= new int[3];
+		a[0]=4;
+		a[1]=3;
+		a[2]= a[0]+a[1];
+		for(int i=0; i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
